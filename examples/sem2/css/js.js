b4183ad5fb16bb0b225710672f0884568f357a8b
Louis <louis@laettere.ch>
2019-01-13 16:13:56 +0100

    ...

---
+++ examples/sem2/css/js.js
@@ -0,0 +1,3 @@
+<style>selector { property : ...ESCAPE UNTRUSTED DATA BEFORE PUTTING HERE...; } </style>     //property value
+<style>selector { property : "...ESCAPE UNTRUSTED DATA BEFORE PUTTING HERE..."; } </style>   //property value
+<span style="property : ...ESCAPE UNTRUSTED DATA BEFORE PUTTING HERE...">text</span>         //property value
