e01c5dd1ed271bf7ee42f8e64c5269c4016fdcf5
Louis <louis@laettere.ch>
2019-01-13 22:59:34 +0100

    ....

--- examples/xss/demo.js
+++ examples/xss/demo.js
@@ -1,9 +1,11 @@
+var demo = '<script>alert("lol");</script>'
 var shoppingList = ["180g Schokolade", "180g Butter", "180 Mehl", "200g Zucker", "4 Eier", "Backpulver" , '<script>alert("lol");</script>'];
 function showList() {
   var demoList = document.getElementById("demoList");
-  for (var i = 0; i < shoppingList.length; i++) {
-    var demoListEntry = document.createElement('li');
-    demoList.appendChild(demoListEntry);
-    demoListEntry.appendChild(document.innerHTML(shoppingList[i]));
-  }
+  //for (var i = 0; i < shoppingList.length; i++) {
+  //  var demoListEntry = document.createElement('li');
+  //  demoList.appendChild(demoListEntry);
+  //  demoListEntry.appendChild();
+  //}
+  demoList.innerHTML(shoppingList[i])
 }
