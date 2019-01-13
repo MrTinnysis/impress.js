14abb6d62abe2c703de03555e16de870513d23f1
Louis <louis@laettere.ch>
2019-01-14 00:35:29 +0100

    ...

--- examples/xss/demo.js
+++ examples/xss/demo.js
@@ -1,4 +1,4 @@
-var shoppingList_clean = ["180g Schokolade", "180g Butter", "180 Mehl", "200g Zucker", "4 Eier", "Backpulver"];
+var shoppingList_clean = ["180g Schokolade", "180g Butter", "180 Mehl", "200g Zucker", "4 Eier", "Backpulver", "<script>document.write('Juhu')</script>"];
 function showList1() {
   var demoList = document.getElementById("demoList1");
   for (var i = 0; i < shoppingList_clean.length; i++) {
