91e691aa9b347c4fcd2dc886f089664273576079
Louis <louis@laettere.ch>
2019-01-13 23:34:08 +0100

    ...

--- examples/xss/demo.js
+++ examples/xss/demo.js
@@ -1,4 +1,5 @@
 var demo = '<script>alert("lol");</script>'
+document.getElementById("inhalt").innerHTML = demo;
 var shoppingList = ["180g Schokolade", "180g Butter", "180 Mehl", "200g Zucker", "4 Eier", "Backpulver" , '<script>alert("lol");</script>'];
 function showList() {
   var demoList = document.getElementById("demoList");
@@ -15,5 +16,5 @@ function add(){
   var add2 = document.getElementById("input");
 alert("Sie haben: "+add2.value+" hinzugefügt!")
   var element = document.getElementById("inhalt");
-  document.getElementById("inhalt").innerHTML = add2.value;
+  document.getElementById("inhalt").innerHTML = demo;
 }
